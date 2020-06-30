350d4f9f706bdf4fa54c1874d665d13b7b919a10
daniela <1469@holbertonschool.com>
2020-06-30 12:31:57 +0000

    Leaving the best version of basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -9,17 +9,20 @@ class BaseModel:
     """Base class that defines all attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Constructor method"""
-        format_date = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if len(kwargs) != 0:
-            for key, val kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(val, format_date)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.__dict__[key] = val
+                    self.__dict__[key] = kwargs[key]
         else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
