a6aced5cd19f9899245e91ba05f04e62049e0107
Marisol2201 <1529@holbertonschool.com>
2020-06-30 16:43:59 +0000

    base_model conditions fixed with test

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,10 @@ class BaseModel:
         """Constructor method"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                if key not in ['__class__']:
+                if '__class__' != key:
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
@@ -40,7 +37,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values of __dict__ of the
         instance"""
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = type(self).__name__
         new_dict["created_at"] = new_dict["created_at"].isoformat()
         new_dict["updated_at"] = new_dict["updated_at"].isoformat()
-        new_dict["__class__"] = type(self).__name__
         return new_dict
