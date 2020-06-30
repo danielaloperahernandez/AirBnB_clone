824b83d07c35a5494591a40fb43a830d2216f0d5
daniela <1469@holbertonschool.com>
2020-06-30 12:16:12 +0000

    Fixing BaseModel __init__

--- models/base_model.py
+++ models/base_model.py
@@ -9,21 +9,16 @@ class BaseModel:
     """Base class that defines all attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Constructor method"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        format_date = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, val kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, format_date)
                 else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+                    self.__dict__[key] = val
 
     def __str__(self):
         """string representation"""
